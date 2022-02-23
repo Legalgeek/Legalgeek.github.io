499c2270f93e145a402c21e9d5b860f01b572d16
Legalgeek <legalgeekmail@gmail.com>
2022-02-23 15:36:52 +0800

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+(function () {
+  // 下方的 cx 的值替换为你的搜索引擎 ID ，可以在搜索引擎-设置-基本设置中获取。
+  var cx = "8102ef80c27d61a80";
+  var gcse = document.createElement("script");
+  gcse.type = "text/javascript";
+  gcse.async = true;
+  gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
